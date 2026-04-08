ec65f4d081705b5f711fbdc0b9451acd07cf1fb5
Viktoria Rumina <viktoriarumina510@gmail.com>
2026-04-08 12:30:42 +0300

    fix(alembic): correct migration revision chain

--- alembic/versions/a0e51b9d2a08_auto.py
+++ alembic/versions/a0e51b9d2a08_auto.py
@@ -1,7 +1,7 @@
 """auto
 
 Revision ID: a0e51b9d2a08
-Revises: 0001_initial
+Revises: 0002_add_base_public_models
 Create Date: 2026-03-23 12:25:51.027948
 """
 from __future__ import annotations
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'a0e51b9d2a08'
-down_revision = '0001_initial'
+down_revision = '0002_add_base_public_models'
 branch_labels = None
 depends_on = None
 
