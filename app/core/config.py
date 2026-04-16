0b45749ea03578c71b128f0f9ad1667e2845caab
rumina-v <viktoriarumina510@gmail.com>
2026-04-16 14:46:27 +0300

    fix(db): edited config.py (#37)

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,21 @@
 from functools import lru_cache
+<<<<<<< HEAD
+=======
 from urllib.parse import quote_plus
+>>>>>>> 08930956ef67672137091fd869b29f98a0be7312
 
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import URL
 
 
 class Settings(BaseSettings):
-    postgres_host: str = Field('localhost', alias='POSTGRES_HOST')
-    postgres_port: int = Field(5432, alias='POSTGRES_PORT')
-    postgres_db: str = Field('capacity_planning', alias='POSTGRES_DB')
-    postgres_user: str = Field('postgres', alias='POSTGRES_USER')
-    postgres_password: str = Field('postgres', alias='POSTGRES_PASSWORD')
-    database_echo: bool = Field(False, alias='DB_ECHO')
+    postgres_scheme: str = 'postgresql+asyncpg'
+    postgres_host: str = 'localhost'
+    postgres_port: int = 5432
+    postgres_db: str = 'capacity_planning'
+    postgres_user: str = 'postgres'
+    postgres_password: str = 'postgres'
+    database_echo: bool = False
 
     model_config = SettingsConfigDict(
         env_file='.env',
@@ -20,11 +24,22 @@ class Settings(BaseSettings):
     )
 
     @property
+<<<<<<< HEAD
+    def database_url(self) -> URL:
+        return URL.create(
+            drivername=self.postgres_scheme,
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            database=self.postgres_db,
+=======
     def database_url(self) -> str:
         return (
             'postgresql+asyncpg://'
             f'{quote_plus(self.postgres_user)}:{quote_plus(self.postgres_password)}'
             f'@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}'
+>>>>>>> 08930956ef67672137091fd869b29f98a0be7312
         )
 
 
